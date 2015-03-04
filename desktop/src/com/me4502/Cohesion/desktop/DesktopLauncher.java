e2b3919f07be8a8f47ca6040869d361f2f10fa78
unknown <100255@HRMYT16.internal.princeofpeace.qld.edu.au>
2015-03-04 11:33:13 +1000

    Pre-Setup Commit

---
+++ desktop/src/com/me4502/Cohesion/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.me4502.Cohesion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.me4502.Cohesion.Cohesion;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Cohesion(), config);
+	}
+}
---
+++ html/src/com/me4502/Cohesion/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.me4502.Cohesion.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.me4502.Cohesion.Cohesion;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Cohesion();
+        }
+}

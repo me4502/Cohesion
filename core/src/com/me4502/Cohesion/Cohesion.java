e5a30f75e1a0fd14f8abe97a47c199da0f5d9cd6
unknown <100255@HRMYT16.internal.princeofpeace.qld.edu.au>
2015-03-04 11:59:50 +1000

    Create an FBO

--- core/src/com/me4502/Cohesion/Cohesion.java
+++ core/src/com/me4502/Cohesion/Cohesion.java
@@ -1,27 +1,52 @@
 package com.me4502.Cohesion;
 
+import java.util.Random;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 
 public class Cohesion extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
-	
+
+	FrameBuffer buffer;
+	OrthographicCamera camera;
+
+	public static final Random RANDOM = new Random();
+
 	@Override
 	public void create () {
+
+		buffer = new FrameBuffer(Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, true);
+
+		camera = new OrthographicCamera(buffer.getWidth(), buffer.getHeight());
+
 		batch = new SpriteBatch();
+
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+		buffer.begin();
+		Gdx.gl.glClearColor(0, 0, 0, 0.4f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.setProjectionMatrix(camera.combined);
+		batch.draw(img, RANDOM.nextInt(300), RANDOM.nextInt(300));
+		batch.end();
+		buffer.end();
+
+		batch.begin();
+		batch.draw(buffer.getColorBufferTexture(), 0, 0);
 		batch.end();
 	}
 }
--- desktop/src/com/me4502/Cohesion/desktop/DesktopLauncher.java
+++ desktop/src/com/me4502/Cohesion/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.me4502.Cohesion.Cohesion;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
 		new LwjglApplication(new Cohesion(), config);
 	}
 }

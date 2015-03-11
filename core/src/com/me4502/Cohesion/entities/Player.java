19148def3e39743c2b3ae1f9237d6b19b74286ba
Madeline Miller <mnmiller1@me.com>
2015-03-11 12:48:26 +1000

    Added player class.

---
+++ core/src/com/me4502/Cohesion/entities/Player.java
@@ -0,0 +1,11 @@
+package com.me4502.Cohesion.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Player extends Entity {
+
+	public Player(Sprite sprite) {
+		super(sprite);
+	}
+
+}

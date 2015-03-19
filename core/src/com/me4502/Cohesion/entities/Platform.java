c7740d78478f0b0dbde53f446f27747b4cfd3dc1
unknown <100255@HRLIB2-11.internal.princeofpeace.qld.edu.au>
2015-03-20 09:42:37 +1000

    Revert "Switch to flow vectors. I can't stand mutable vectors."
    This reverts commit bd25b822b85d0621f17d2a942149641317ff3922.

--- core/src/com/me4502/Cohesion/entities/Platform.java
+++ core/src/com/me4502/Cohesion/entities/Platform.java
@@ -1,12 +1,12 @@
 package com.me4502.Cohesion.entities;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.flowpowered.math.vector.Vector2d;
+import com.badlogic.gdx.math.Vector2;
 import com.me4502.Cohesion.map.MapInstance;
 
 public class Platform extends Entity {
 
-	public Platform(MapInstance map, Sprite sprite, Vector2d position) {
+	public Platform(MapInstance map, Sprite sprite, Vector2 position) {
 		super(map, sprite, position);
 	}
 

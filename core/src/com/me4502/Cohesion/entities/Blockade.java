4d939c3e12e331c463e784b59ed48c9f3c128074
unknown <100255@HRLIB2-11.internal.princeofpeace.qld.edu.au>
2015-03-30 14:11:57 +1000

    Missed files

---
+++ core/src/com/me4502/Cohesion/entities/Blockade.java
@@ -0,0 +1,24 @@
+package com.me4502.Cohesion.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.me4502.Cohesion.map.MapInstance;
+
+public class Blockade extends Entity {
+
+	public Blockade(MapInstance map, Sprite sprite, Vector2 position) {
+		super(map, sprite, position);
+	}
+
+	public void onCollision(Entity ent) {
+		if(ent instanceof Projectile && !((Projectile) ent).hasCollided) {
+			this.velocity.add(ent.velocity.cpy().scl(ent.collisionDrag));
+			((Projectile)ent).hasCollided = true;
+		}
+	}
+
+	@Override
+	public boolean hasGravity() {
+		return true;
+	}
+}
